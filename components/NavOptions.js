5dab94d1f3b9c073151bcac4c54be0378d9b60ec
kendrik <kendrik_lim@artc.a-star.edu.sg>
2021-11-07 16:31:36 +0800

    Implementing react native navigation

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { FlatList, Image, Text, TouchableOpacity, View } from "react-native";
 import tw from "tailwind-react-native-classnames";
+import { Icon } from "react-native-elements/dist/icons/Icon";
 
 const data = [
   {
@@ -30,7 +31,13 @@ const NavOptions = () => {
               style={{ width: 120, height: 120, resizeMode: "contain" }}
               source={{ uri: item.image }}
             />
-            <Text>{item.title}</Text>
+            <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
+            <Icon
+              style={tw`p-2 bg-black rounded-full w-10 mt-4`}
+              name="arrowright"
+              color="white"
+              type="antdesign"
+            />
           </View>
         </TouchableOpacity>
       )}
