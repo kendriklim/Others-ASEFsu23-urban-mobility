20af1dc37bb5e38b09d8575bfc6d7427ab545d39
kendrik <kendrik_lim@artc.a-star.edu.sg>
2021-11-07 15:55:54 +0800

    Update nav options v1

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,25 @@
 import React from "react";
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView, Image } from "react-native";
+import tw from "tailwind-react-native-classnames";
+import NavOptions from "../components/NavOptions";
 
 const HomeScreen = () => {
   return (
-    <SafeAreaView>
-      <Text style={styles.text}>I am the homescreen</Text>
+    <SafeAreaView style={tw`bg-white h-full`}>
+      <View style={tw`p-5`}>
+        <Image
+          style={{
+            width: 100,
+            height: 100,
+            resizeMode: "contain",
+          }}
+          source={{
+            uri: "https://links.papareact.com/gzs",
+          }}
+        />
+
+        <NavOptions />
+      </View>
     </SafeAreaView>
   );
 };
@@ -12,7 +27,7 @@ const HomeScreen = () => {
 export default HomeScreen;
 
 const styles = StyleSheet.create({
-    text: {
-        color: 'blue'
-    }
+  text: {
+    color: "blue",
+  },
 });
