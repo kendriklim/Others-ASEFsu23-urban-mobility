e3d04eadae395416f797727e3dbe303d42dd6b39
kendrik <kendrik_lim@artc.a-star.edu.sg>
2021-11-09 15:55:58 +0800

    working prototype v0.2

--- components/Maps.js
+++ components/Maps.js
@@ -1,63 +1,107 @@
-import React, { useEffect } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useRef } from "react";
 import MapView, { Marker } from "react-native-maps";
-import { useSelector } from "react-redux";
+import { Icon } from "react-native-elements";
+import { useNavigation } from "@react-navigation/native";
+import { View, TouchableOpacity } from "react-native";
+
 import tw from "tailwind-react-native-classnames";
-import { selectDestination, selectOrigin } from "../slices/navSlice";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  selectDestination,
+  selectOrigin,
+  setTravelTimeInformation,
+} from "../slices/navSlice";
 import MapViewDirections from "react-native-maps-directions";
 import { GOOGLE_MAPS_APIKEY } from "@env";
-import { useRef } from "react";
 
-const Maps = () => {
+const Map = () => {
   const origin = useSelector(selectOrigin);
   const destination = useSelector(selectDestination);
+  const navigation = useNavigation();
+  const dispatch = useDispatch();
+
   const mapRef = useRef(null);
 
   useEffect(() => {
     if (!origin || !destination) return;
 
-    // Zoom and fit to markers
-    mapRef.current?.fitToSuppliedMarkers(["origin", "destination"], {
+    mapRef.current.fitToSuppliedMarkers(["origin", "destination"], {
       edgePadding: { top: 50, right: 50, bottom: 50, left: 50 },
     });
   }, [origin, destination]);
 
+  useEffect(() => {
+    if (!origin || !destination) return;
+
+    const getTravelTime = async () => {
+      fetch(
+        `https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=${origin.description}&destinations=${destination.description}&key=${GOOGLE_MAPS_APIKEY}`
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          dispatch(setTravelTimeInformation(data.rows[0].elements[0]));
+        });
+    };
+
+    getTravelTime();
+  }, [origin, destination, GOOGLE_MAPS_APIKEY]);
+
   return (
-    <MapView
-      ref={mapRef}
-      style={tw`flex-1`}
-      mapType="mutedStandard"
-      initialRegion={{
-        latitude: origin.location.lat,
-        longitude: origin.location.lng,
-        latitudeDelta: 0.005,
-        longitudeDelta: 0.005,
-      }}
-    >
-      {origin && destination && (
-        <MapViewDirections
-          origin={origin.description}
-          destination={destination.description}
-          apikey={"AIzaSyDgn5900zen3QjId3BBxov06BkvuLNl9jw"}
-          strokeWidth={3}
-          strokeColor="black"
-        />
-      )}
-      {origin?.location && (
-        <Marker
-          coordinate={{
-            latitude: origin.location.lat,
-            longitude: origin.location.lng,
-          }}
-          title="Origin"
-          description={origin.description}
-          identifier="origin"
-        />
-      )}
-    </MapView>
+    <View style={tw`flex-1`}>
+      <TouchableOpacity
+        onPress={() => navigation.goBack()}
+        style={tw`absolute z-10 p-4`}
+      >
+        <Icon name="arrow-back-circle" type="ionicon" color="black" size={38} />
+      </TouchableOpacity>
+      <MapView
+        ref={mapRef}
+        mapType="mutedStandard"
+        style={tw`w-full h-full m-0 p-0 `}
+        initialRegion={{
+          latitude: origin.location.lat,
+          longitude: origin.location.lng,
+          latitudeDelta: 0.005,
+          longitudeDelta: 0.005,
+        }}
+      >
+        {origin && destination && (
+          <MapViewDirections
+            lineDashPattern={[1]}
+            origin={origin.description}
+            destination={destination.description}
+            apikey="AIzaSyDgn5900zen3QjId3BBxov06BkvuLNl9jw"
+            strokeWidth={3}
+            strokeColor="black"
+          />
+        )}
+
+        {origin?.location && (
+          <Marker
+            coordinate={{
+              latitude: origin.location.lat,
+              longitude: origin.location.lng,
+            }}
+            title="Origin"
+            description={origin.description}
+            identifier="origin"
+          />
+        )}
+
+        {destination?.location && (
+          <Marker
+            coordinate={{
+              latitude: destination.location.lat,
+              longitude: destination.location.lng,
+            }}
+            title="Destination"
+            description={destination.description}
+            identifier="destination"
+          />
+        )}
+      </MapView>
+    </View>
   );
 };
 
-export default Maps;
-
-const styles = StyleSheet.create({});
+export default Map;
--- components/NavFavourites.js
+++ components/NavFavourites.js
@@ -1,36 +1,107 @@
 import React from "react";
-import { FlatList, StyleSheet, Text, View } from "react-native";
-import { TouchableOpacity } from "react-native-gesture-handler";
+import { SafeAreaView, View, Text, TouchableOpacity } from "react-native";
+import { FlatList } from "react-native-gesture-handler";
+import tw from "tailwind-react-native-classnames";
+import { Icon } from "react-native-elements";
+
+import { useDispatch, useSelector } from "react-redux";
+import { selectOrigin, setDestination, setOrigin } from "../slices/navSlice";
+import { useNavigation, useRoute } from "@react-navigation/native";
 
 const data = [
   {
-    id: "123",
+    id: "7",
     icon: "home",
-    location: "Home",
-    destination: "Code Street, London, UK",
+    label: "Home",
+    location: {
+      lat: 51.522392,
+      lng: -0.07083420000000001,
+    },
+    description: "Code Street, London, UK",
   },
   {
-    id: "456",
+    id: "8",
     icon: "briefcase",
-    location: "Work",
-    destination: "London Eye, London, UK",
+    label: "Work",
+    location: {
+      lat: 51.5032973,
+      lng: -0.1195537,
+    },
+    description: "London Eye, London, UK",
+  },
+  {
+    id: "9",
+    icon: "school",
+    label: "Library",
+    location: {
+      lat: 51.4995,
+      lng: -0.1034399,
+    },
+    description: "Library Street, London, UK",
   },
 ];
 
 const NavFavourites = () => {
+  const dispatch = useDispatch();
+  const navigation = useNavigation();
+
+  const origin = useSelector(selectOrigin);
+
+  const route = useRoute();
+
   return (
-    <FlatList
-      data={data}
-      keyExtractor={(item) => item.id}
-      renderItem={({ item }) => (
-        <TouchableOpacity>
-          <Text>Favourites here</Text>
-        </TouchableOpacity>
-      )}
-    />
+    <SafeAreaView>
+      <FlatList
+        style={tw`px-6`}
+        data={
+          route.name !== "HomeScreen"
+            ? data.filter((fav) => fav.description !== origin?.description)
+            : data
+        }
+        keyExtractor={(item) => item.id}
+        renderItem={({ item: { label, description, location, icon } }) => (
+          <TouchableOpacity
+            style={tw`flex-row items-center rounded-full bg-white mb-2 p-2`}
+            onPress={() => {
+              if (route.name === "HomeScreen") {
+                dispatch(
+                  setOrigin({
+                    location: location,
+                    description: description,
+                  })
+                );
+
+                navigation.navigate("MapScreen");
+              }
+
+              if (route.name === "NavigateCard") {
+                dispatch(
+                  setDestination({
+                    location: location,
+                    description: description,
+                  })
+                );
+
+                navigation.navigate("RideOptionsCard");
+              }
+            }}
+          >
+            <Icon
+              style={tw`mr-4 rounded-full bg-gray-500 p-2`}
+              name={icon}
+              type="ionicon"
+              color="white"
+              size={18}
+            />
+            <View>
+              <Text style={tw`font-semibold`}>{label}</Text>
+              <Text>{description}</Text>
+            </View>
+          </TouchableOpacity>
+        )}
+      />
+    </SafeAreaView>
   );
 };
 
 export default NavFavourites;
-
-const styles = StyleSheet.create({});
