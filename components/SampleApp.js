f186f2349ae75010ab54464131f09394056551a5
Eric Nguyen <62949169+Eric-nguyen1402@users.noreply.github.com>
2021-11-12 22:08:33 +0800

    Add files via upload

--- components/SampleApp.js
+++ components/SampleApp.js
@@ -28,7 +28,7 @@ const NavOptions = ({}) => {
           marginLeft:100,
           marginBottom:40
         }}
-        onPress={() => { navigation.navigate("VolunteerScreen"); }}
+        onPress={() => { navigation.navigate("CallScreen"); }}
     >
       <Icon name="alert-outline" type="ionicon" color="white"  size={30} /><Text    style={{ marginLeft:10 ,marginRight:10,textAlign:"center" ,textDecorationStyle:"solid",color:"white"
         }} >RESCUE VOLUNTEER BULAO</Text>
